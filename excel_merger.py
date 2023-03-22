0cb663761d92497be6c46c967d3d545c554355a6
Ivan Drygin <idrygin@gmail.com>
2023-03-22 23:24:56 +0300

    some refactroing

--- excel_merger.py
+++ excel_merger.py
@@ -6,14 +6,13 @@ import sys
 file1 = sys.argv[1]
 file2 = sys.argv[2]
 
-# Read the first Excel document into a pandas DataFrame
+# Read the first Excel document into a pandas DataFrame and remove some columns
 df1 = pd.read_excel(file1).drop(['Footprint'], axis=1)
 
-# Read the second Excel document into a pandas DataFrame
+# Read the second Excel document into a pandas DataFrame and remove some columns
 df2 = pd.read_excel(file2).drop(['Name'], axis=1)
 
-# Merge the two DataFrames on the 'Reference' column, keeping all columns from the second DataFrame except for
-# 'Footprint' and 'Name'
+# Merge the two DataFrames on the 'Reference' column, keeping all columns from the second DataFrame
 merged_df = pd.merge(df1, df2, on='Reference', how='right')
 
 print(merged_df)
